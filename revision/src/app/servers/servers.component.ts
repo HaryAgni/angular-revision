928077f562d097ddf646ba3229fbadb8d42da257
Harshit Agnihotri <haryagni123@gmail.com>
2024-09-17 22:48:46 +0530

    event binding and two way data binding

--- revision/src/app/servers/servers.component.ts
+++ revision/src/app/servers/servers.component.ts
@@ -13,10 +13,20 @@ import { Component } from '@angular/core';
 export class ServersComponent {
 
   allowNewServer = false;
+  serverCreationStatus = 'No server created'
+  serverName="Server Name";
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created";
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
   }
 
 }
