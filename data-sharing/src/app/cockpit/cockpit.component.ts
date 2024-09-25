d76ad948ebe6e92b8d51b526b1ccb3ab5ca9ba8e
Harshit Agnihotri <haryagni123@gmail.com>
2024-09-25 23:12:01 +0530

    component databinding, using view child and using all angular lifecycle hooks

--- data-sharing/src/app/cockpit/cockpit.component.ts
+++ data-sharing/src/app/cockpit/cockpit.component.ts
@@ -1,34 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
-  styleUrl: './cockpit.component.css',
+  styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent {
-  @Output() serverCreated = new EventEmitter<{
-    serverName: string;
-    serverContent: string;
-  }>();
-  @Output('bpCreated') bluePrintCreated = new EventEmitter<{
-    serverName: string;
-    serverContent: string;
-  }>();
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: false }) serverContentInput: ElementRef;
 
-  newServerName = '';
-  newServerContent = '';
+  constructor() { }
 
-  onAddServer() {
+  ngOnInit() {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
-    this.bluePrintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
+
 }
